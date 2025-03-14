fd9dda861a07c472ee0655c6c9c47abadd5e7f17
Noa Lea <noa@noalea.work>
2025-03-14 13:08:57 -0400

    Fix android build

--- apps/tickermoves/babel.config.js
+++ apps/tickermoves/babel.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'react-native-dotenv',
-    {
-      moduleName: '@env',
-      path: '.env',
-    },
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
   ],
 };
